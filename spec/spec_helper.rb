c1f8f53feefd9ad4c5c6a2c731dd86cb4b0b74d7
Zach Latta <zchlatta@gmail.com>
2013-05-23 16:54:36 -0700

    Move Coveralls.wear! to the very top of spec_helper.rb.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,10 @@
+require 'coveralls'
+Coveralls.wear!
+
 require_relative '../lib/octonore'
 
 require 'webmock/rspec'
 require 'vcr'
-require 'coveralls'
-Coveralls.wear!
 
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/octonore_cassettes'
