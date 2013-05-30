49e6f3dc6d043ecb8ad0d2f644870aca0e175368
Zach Latta <zchlatta@gmail.com>
2013-05-30 04:33:11 -0700

    Fix list method. Revise documentation. Make valid_template_hash? protected.

--- lib/octonore/template.rb
+++ lib/octonore/template.rb
@@ -6,6 +6,17 @@ module Octonore
 
     attr_accessor :name, :source
 
+    # List available templates
+    #
+    # Example:
+    #   >> Octonore::Template.list
+    #
+    # @return [Array] The available gitignore templates.
+    def self.list
+      raw_list = Octonore::HTTPHelper.instance.get("/templates")
+      parsed_list = raw_list.parsed_response
+    end
+
     # Create a new template.
     #
     # Example:
@@ -14,6 +25,7 @@ module Octonore
     #
     # @param name [String] name of template to create,
     #                      case sensitive
+    # @return [String] Contents of template.
     def initialize(name)
       self.name = name
       reload
@@ -28,6 +40,8 @@ module Octonore
     #   => "# Object files\n*.o\n\n# Libraries\n*.lib..."
     #   >> outdated_template.source
     #   => "# Object files\n*.o\n\n# Libraries\n*.lib..."
+    #
+    # @return [String] Contents of template.
     def reload
       data = get_template_hash @name
       
@@ -39,26 +53,26 @@ module Octonore
       end
     end
 
-    # List available templates
+
+    protected
+
+    # Verify that the template doesn't contain "Not Found"
     #
-    # Example:
-    #   >> Octonore::Template.list
-    def self.list
-      Octonore::HTTPHelper.instance.get("/templates")
+    # @param [Hash] The template's hash.
+    # @return [Boolean] Whether the template doesn't contain "Not Found"
+    def valid_template_hash?(template_hash)
+      template_hash["message"] != "Not Found"
     end
 
 
     private
 
     # Get the specified template's hash from Github.
+    # 
     # @param name [String] name of template to get
     # @return [Hash] hash containing template info
     def get_template_hash(name)
       Octonore::HTTPHelper.instance.get("/templates/#{name}")
-    end
-
-    def valid_template_hash?(template_hash)
-      template_hash["message"] != "Not Found"
     end
 
   end
