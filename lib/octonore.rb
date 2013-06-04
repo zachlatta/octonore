2b61b3e82c5ef8ac83705f079e0651d496828840
Zach Latta <zchlatta@gmail.com>
2013-06-04 16:24:11 -0700

    Refactor require code.

--- lib/octonore.rb
+++ lib/octonore.rb
@@ -1,5 +1,10 @@
 require 'httparty'
 
-Dir[File.dirname(__FILE__) + '/octonore/*.rb'].each do |file|
-  require file
+def require_all(path)
+  glob = File.join(File.dirname(__FILE__), path, '*.rb')
+  Dir[glob].each do |f|
+    require f
+  end
 end
+
+require_all 'octonore'
